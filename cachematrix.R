f690ee6abb2e127cd60bff41d7665ab9c26f8cab
attackgnome <attackgnome@users.noreply.github.com>
2020-04-23 20:41:13 -0600

    Week 3 Assignment Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a class of matrix that can cache its inverse and the 
+## corresponding function solve for the inverse of that matrix
+## using ginv() from the MASS package
 
-## Write a short comment describing this function
+require(MASS)
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  require(MASS)  
+  m <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
 
+  get <- function() x
+  setinverse <- function(ginv) m <<- ginv
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -12,4 +28,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- ginv(data, ...)
+  x$setinverse(m)
+  m
+  
+  
+  
 }
